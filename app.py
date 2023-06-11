002b8b57baf56c353d1b66481890a942604d99b1
alearal12 <alejandro.araya@ucrso.info>
2023-06-11 11:08:56 -0600

    webpage working, global weight not working

--- app.py
+++ app.py
@@ -1,116 +1,102 @@
 from flask import Flask, render_template, request
 import numpy as np
 from scipy.optimize import linear_sum_assignment
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-@app.route('/')
+pesos_predefinidos = {
+    'derrumbes': 1.0,
+    'gravedad de accidente': 2.0,
+    'duración': 1.5,
+    'estado del tiempo': 1.0,
+    'congestión vial': 1.0,
+    'estado de la carretera': 1.0
+}
+
+rutas_predefinidas = {
+    2020: np.array([[1.0, 2.0, 3.0, 4.0, 2.5, 3.0],
+                    [3.0, 1.5, 2.5, 1.0, 4.0, 2.0],
+                    [2.5, 3.0, 1.0, 2.0, 1.5, 2.5],
+                    [1.5, 2.0, 1.5, 3.0, 2.0, 4.0]]),
+    2021: np.array([[2.0, 1.5, 3.0, 2.5, 1.0, 4.0],
+                    [3.0, 2.0, 1.0, 2.5, 3.0, 1.5],
+                    [1.0, 3.0, 2.5, 1.5, 2.5, 3.0],
+                    [2.5, 4.0, 1.5, 2.0, 1.0, 2.0]])
+}
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pesos_predefinidos=pesos_predefinidos)
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
 
-    # Hardcoded values for the first two years
-    derrumbes_first_year = 10.0
-    gravedad_first_year = 5.0
-    duracion_first_year = 20.0
-    tiempo_first_year = 2.0
-    congestion_first_year = 1.0
-    estado_first_year = 2.0
-
-    derrumbes_second_year = 15.0
-    gravedad_second_year = 8.0
-    duracion_second_year = 18.0
-    tiempo_second_year = 3.0
-    congestion_second_year = 2.0
-    estado_second_year = 1.0
-
-    peso_value = request.form.get('peso')
-
-    if peso_value is not None:
-        peso = float(peso_value)
-    else:
-        peso = 0.0  # Default value if input is not provided
-
-    derrumbes_last_year = request.form.get('derrumbes_last_year')
-    if derrumbes_last_year:
-        derrumbes = float(derrumbes_last_year)
-    else:
-        derrumbes = 0.0  # Default value if input is not provided
-
-    gravedad_last_year = request.form.get('gravedad_last_year')
-    if gravedad_last_year:
-        gravedad = float(gravedad_last_year)
-    else:
-        gravedad = 0.0  # Default value if input is not provided
-
-    duracion_last_year = request.form.get('duracion_last_year')
-    if duracion_last_year:
-        duracion = float(duracion_last_year)
-    else:
-        duracion = 0.0  # Default value if input is not provided
-
-    tiempo_last_year = request.form.get('tiempo_last_year')
-    if tiempo_last_year:
-        tiempo = float(tiempo_last_year)
-    else:
-        tiempo = 0.0  # Default value if input is not provided
-
-    congestion_last_year = request.form.get('congestion_last_year')
-    if congestion_last_year:
-        congestion = float(congestion_last_year)
-    else:
-        congestion = 0.0  # Default value if input is not provided
-
-    estado_last_year = request.form.get('estado_last_year')
-    if estado_last_year:
-        estado = float(estado_last_year)
-    else:
-        estado = 0.0  # Default value if input is not provided
-
+@app.route('/results', methods=['POST'])
+def results():
     pesos = {}
-    entradas = ['derrumbes', 'gravedad de accidente', 'duración', 'estado del tiempo', 'congestión vial', 'estado de la carretera']
 
-    pesos_globales = np.zeros(len(entradas))
+    # Obtener los pesos para el último año ingresados por el usuario
+    for entrada in pesos_predefinidos:
+        try:
+            peso = float(request.form[entrada])
+            if peso < 0:
+                return render_template('error.html', message="El peso no puede ser negativo. Intente nuevamente.")
+            if entrada == 'estado de la carretera' and peso > 3:
+                return render_template('error.html',
+                                       message="El valor del estado de la carretera no puede ser mayor a 3. Intente nuevamente.")
+            if entrada == 'congestión vial' and peso > 3:
+                return render_template('error.html',
+                                       message="El valor de la congestión vial no puede ser mayor a 3. Intente nuevamente.")
+            pesos[entrada] = peso
+        except ValueError:
+            return render_template('error.html', message="Valor inválido. Intente nuevamente.")
 
-    for entrada in entradas:
-        peso_value = request.form.get(entrada)
-        peso = float(peso_value)
-        pesos[entrada] = peso
-        pesos_globales += peso
-
+    # Generar la matriz de entrada para el último año
     matriz = np.zeros((4, 6))
-
     for i in range(4):
-        for j, entrada in enumerate(entradas):
-            valor_value = request.form.get(f'ruta_{i+1}_{entrada}')
-            valor = float(valor_value)
-            matriz[i, j] = valor
+        for j, entrada in enumerate(pesos_predefinidos):
+            try:
+                valor = float(request.form[f"ruta{i + 1}_{entrada}"])
+                if valor < 0:
+                    return render_template('error.html', message="El valor no puede ser negativo. Intente nuevamente.")
+                if entrada in ['estado de la carretera', 'congestión vial'] and valor > 3:
+                    return render_template('error.html',
+                                           message=f"El valor de {entrada} no puede ser mayor a 3. Intente nuevamente.")
+                matriz[i, j] = valor
+            except ValueError:
+                return render_template('error.html', message="Valor inválido. Intente nuevamente.")
 
-    # Hardcoded values for the first two years
-    matriz[0] = [10.0, 5.0, 20.0, 2.0, 1.0, 2.0]
-    matriz[1] = [15.0, 8.0, 18.0, 3.0, 2.0, 1.0]
+    # Calcular el total del peso de cada ruta por año
+    rutas_optimas = {}
+    for year, rutas_predefinidas_matriz in rutas_predefinidas.items():
+        total_pesos = []
+        for i in range(4):
+            peso_ruta = np.sum(rutas_predefinidas_matriz[i] * np.array(list(pesos.values())))
+            total_pesos.append(peso_ruta)
+        fila_indices, columna_indices = linear_sum_assignment(rutas_predefinidas_matriz)
+        ruta_optima = np.argmin(total_pesos)
+        rutas_optimas[year] = ruta_optima + 1
 
-    total_pesos = []
+    # Calcular el total del peso de cada ruta para el último año
+    total_pesos_ultimo = []
     for i in range(4):
         peso_ruta = np.sum(matriz[i] * np.array(list(pesos.values())))
-        total_pesos.append(peso_ruta)
+        total_pesos_ultimo.append(peso_ruta)
 
-    ruta_optima = np.argmin(total_pesos)
-    peso_optimo = total_pesos[ruta_optima]
+    # Resolver el problema de asignación utilizando el método húngaro para el último año
+    fila_indices_ultimo, columna_indices_ultimo = linear_sum_assignment(matriz)
 
-    # Create a bar chart of the weights
-    plt.bar(list(pesos.keys()), list(pesos.values()))
-    plt.xlabel('Entradas')
-    plt.ylabel('Pesos')
-    plt.title('Pesos de las entradas')
-    plt.savefig('static/chart.png')  # Save the chart as an image file
-    plt.close()
+    # Imprimir la ruta más óptima y su peso para el último año
+    ruta_optima_ultimo = np.argmin(total_pesos_ultimo)
+    peso_optimo_ultimo = total_pesos_ultimo[ruta_optima_ultimo]
 
-    return render_template('results.html', ruta_optima=ruta_optima+1, peso_optimo=peso_optimo)
+    # Calcular la ruta óptima global
+    rutas_optimas[2022] = ruta_optima_ultimo + 1
+    rutas_optimas = dict(sorted(rutas_optimas.items()))
+    peso_optimo_global = min(total_pesos_ultimo)
+    ruta_optima_global = np.argmin(total_pesos_ultimo) + 1
+
+    return render_template('results.html', rutas_optimas=rutas_optimas,
+                           ruta_optima_global=ruta_optima_global, peso_optimo_global=peso_optimo_global)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
